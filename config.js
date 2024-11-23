e7579a5d77d6b89cf5c02ffd16d7922c8cf60023
victor <medeirosvictor401@gmail.com>
2024-11-23 19:02:53 -0300

    Descrição do commit

---
+++ config.js
@@ -0,0 +1,14 @@
+
+    const playerNameInput = document.getElementById('nome-jogador');
+    if (playerNameInput) {
+        playerNameInput.addEventListener('input', function() {
+            const playerName = this.value;
+            if (playerName) {
+                document.querySelector('h1').innerText = `Bem-vindo, ${playerName}!`;
+            } else {
+                document.querySelector('h1').innerText = 'Informações do Meu Perfil';
+            }
+        });
+    }
+
+
