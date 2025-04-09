568c924a2fc8e87eb8340c85f3ce2b8642e268b3
Hyan Nagle <naglehyan@gmail.com>
2025-04-09 14:51:47 -0300

    att login

--- js/login.js
+++ js/login.js
@@ -3,6 +3,11 @@ function login(){
     // e.preventDefault();
     const usuario = document.getElementById("email").value
     const senha = document.getElementById("senha").value
+    const teste = document.getElementById("teste")
+    const teste1 = document.getElementById("teste1")
+    const loginRealizado = document.querySelector(".loginRealizado")
+    const emailErro = document.querySelector(".emailErro")
+    const senhaErro = document.querySelector(".senhaErro")
 
     let RegUsuario = JSON.parse(localStorage.getItem("RegistrodeUsuarios"))
 
@@ -20,20 +25,35 @@ function login(){
     if(checarUsuario.senha == senha){
         senhakey = true
     }else{
-        alert("senha está errada")
+        senhaErro.innerHTML = `<p class="mt-1 mb-1 fs-5 text text-danger">Senha ou email estão incorretos!</p>`
     }
 
     console.log(senhakey)
     console.log(userkey)
 
-    if(userkey === true && senhakey === true){alert("login realizado")
-        localStorage.setItem("UsarioLogado", JSON.stringify)({
+    if(userkey === true && senhakey === true){
+        localStorage.setItem("UsuarioLogado", JSON.stringify(
+        {
             usuario: usuario,
-            tipo: senha,
-        })
-    }
+            senha: senha,
+        }));
 
 
+        senhaErro.innerHTML = ` `
+        loginRealizado.classList.remove("d-none")
+        teste.classList.remove("d-none")
+        teste1.classList.remove("d-none")
+    }    
+}
+
+function logout(){
+    const loginRealizado = document.querySelector(".loginRealizado")
+
+    localStorage.removeItem("UsuarioLogado")
+    loginRealizado.classList.add("d-none")
+    teste.classList.add("d-none")
+    teste1.classList.add("d-none")
 }
 
 document.querySelector("#lgnBtn").addEventListener("click", login)
+document.querySelector(".logout").addEventListener("click", logout)
